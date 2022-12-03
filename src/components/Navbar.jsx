11d3392cba9b327517a35ae3e0af11ed97c17483
enohyn <shayokullah@gmail.com>
2022-12-04 01:12:47 +0600

    Navbar component added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,19 @@ import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
+    <nav className='w-full flex py-6 justify-between items-center container'>
         <img src={logo} alt='logo' className='w-[124px] h-[32px]'/>
-        <ul>
+        <ul className='list-none sm:flex hidden justify-end flex-1 items-center'>
+            <div className='flex justify-around'>
+            {navLinks.map((nav,index)=>(
+                <li key={nav.id} className={`font-poppins font-normal cursor-pointer ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
+                    <a href={`#${nav.id}`} >
+                        {nav.title}
+                    </a>
 
+                </li>
+            ))}
+            </div>
         </ul>
     </nav>
   )
